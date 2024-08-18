3d9b446c987c692ee219935eb2241cac7fa578b5
NotAShelf <raf@notashelf.dev>
2024-08-18 16:50:51 +0300

    modules/openssh: harden

--- modules/openssh.nix
+++ modules/openssh.nix
@@ -1,8 +1,16 @@
-{
+{lib, ...}: let
+  inherit (lib.modules) mkForce;
+in {
   services.openssh = {
     enable = true;
 
     # since this is a VM, use a separate port than the host's
     ports = [2222];
+
+    settings = {
+      PasswordAuthentication = mkForce false;
+      KexAlgorithms = mkForce ["sntrup761x25519-sha512@openssh.com"];
+      KbdInteractiveAuthentication = mkForce false;
+    };
   };
 }
