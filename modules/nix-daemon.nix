14f2d11c75cd2f2340c92f6ff948f5595a4d3fef
NotAShelf <raf@notashelf.dev>
2024-08-18 16:33:48 +0300

    modules: apply statix lints

--- modules/nix-daemon.nix
+++ modules/nix-daemon.nix
@@ -2,18 +2,22 @@
   asGB = size: toString (size * 1024 * 1024 * 1024);
 in {
   nix = {
-    # Hard-link duplicated files
-    settings.auto-optimise-store = true;
-
-    # auto-free the /nix/store
-    settings.min-free = asGB 10;
-    settings.max-free = asGB 50;
-
     # useful for ad-hoc nix-shell's for debugging
     # use mkForce to avoid search path warnings with nix-darwin
     nixPath = pkgs.lib.mkForce ["nixpkgs=${pkgs.path}"];
 
-    gc.automatic = true;
-    gc.options = pkgs.lib.mkDefault "--delete-older-than 14d";
+    # Hard-link duplicated files
+    settings = {
+      auto-optimise-store = true;
+
+      # auto-free the /nix/store
+      min-free = asGB 10;
+      max-free = asGB 50;
+    };
+
+    gc = {
+      automatic = true;
+      options = pkgs.lib.mkDefault "--delete-older-than 14d";
+    };
   };
 }
